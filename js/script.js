f6b100900cf7209679da59e2092ada6bb746fd62
farid10 <faridismayilli@mail.ru>
2017-03-09 09:37:09 +0400

    template

---
+++ js/script.js
@@ -0,0 +1,175 @@
+//multi
+function slide(){
+	var a = document.getElementsByClassName('myFirstButton')[0]
+	a.innerHTML = "YES I WANT "
+
+	var b = document.getElementById('multiPurpose').getElementsByClassName('myFirstButton')[0].style.padding = "24px 27px 22px";
+		
+}
+function edils(){
+		var a = document.getElementsByClassName('myFirstButton')[0].innerHTML = "BUY THIS THEME"
+
+}
+//multi
+
+
+//cleanCode
+
+function goster1(){
+	var a = document.createElement('h1')
+	a.innerHTML = "<b>Clean<b> Code"
+	a.style.color = "white"
+	a.style.fontSize = "80px"
+	var b = document.createElement('p')
+	b.innerHTML = "We tried to make very high-quality product and so our code is very neat and clean. Whatever anyone could improve and modify the template to your liking."
+	b.style.color = "#999"
+	b.style.fontSize = "26px"
+	b.style.marginTop = "-30px"
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(a)
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(b)
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[0].style.background = "white"
+	var c = document.getElementById('cleanCode').getElementsByClassName('firstC')[0]
+	c.style.width = "60px"
+	c.style.height = "60px"
+	c.style.marginTop = "50px"
+	c.style.background = "url(images/i1.png)"
+	var d = document.createElement('h6')
+	d.innerHTML = "Clean Code"
+	d.style.fontSize = "20px"
+	d.style.marginTop = "5px"
+	d.style.marginLeft = "-14px"
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[0].getElementsByTagName('div')[1].appendChild(d)
+}
+function goster2(){
+	var a = document.createElement('h1')
+	a.innerHTML = "<b>Technical<b> Support"
+	a.style.color = "white"
+	a.style.fontSize = "80px"
+	var b = document.createElement('p')
+	b.innerHTML = "We tried to make very high-quality product and so our code is very neat and clean. Whatever anyone could improve and modify the template to your liking."
+	b.style.color = "#999"
+	b.style.fontSize = "26px"
+	b.style.marginTop = "-30px"
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(a)
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(b)
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[1].style.background = "white"
+	var c = document.getElementById('cleanCode').getElementsByClassName('secondC')[0]
+	c.style.width = "60px"
+	c.style.height = "60px"
+	c.style.marginTop = "50px"
+	c.style.background = "url(images/i2.png)"
+	var d = document.createElement('h6')
+	d.innerHTML = "Support"
+	d.style.fontSize = "20px"
+	d.style.marginTop = "5px"
+	d.style.marginLeft = "-14px"
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[1].getElementsByTagName('div')[1].appendChild(d)
+
+	
+}
+function goster3(){
+	var a = document.createElement('h1')
+	a.innerHTML = "<b>Responsive<b> "
+	a.style.color = "white"
+	a.style.fontSize = "80px"
+	var b = document.createElement('p')
+	b.innerHTML = "We tried to make very high-quality product and so our code is very neat and clean. Whatever anyone could improve and modify the template to your liking."
+	b.style.color = "#999"
+	b.style.fontSize = "26px"
+	b.style.marginTop = "-30px"
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(a)
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(b)
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[2].style.background = "white"
+	var c = document.getElementById('cleanCode').getElementsByClassName('thirdC')[0]
+	c.style.width = "60px"
+	c.style.height = "60px"
+	c.style.marginTop = "50px"
+	c.style.background = "url(images/i3.png)"
+	var d = document.createElement('h6')
+	d.innerHTML = "Responsive"
+	d.style.fontSize = "20px"
+	d.style.marginTop = "5px"
+	d.style.marginLeft = "-14px"
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[2].getElementsByTagName('div')[1].appendChild(d)
+
+	
+}
+function goster4(){
+	var a = document.createElement('h1')
+	a.innerHTML = "<b>Documentation<b> "
+	a.style.color = "white"
+	a.style.fontSize = "80px"
+	var b = document.createElement('p')
+	b.innerHTML = "We tried to make very high-quality product and so our code is very neat and clean. Whatever anyone could improve and modify the template to your liking."
+	b.style.color = "#999"
+	b.style.fontSize = "26px"
+	b.style.marginTop = "-30px"
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(a)
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(b)
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[3].style.background = "white"
+	var c = document.getElementById('cleanCode').getElementsByClassName('fourthC')[0]
+	c.style.width = "60px"
+	c.style.height = "60px"
+	c.style.marginTop = "50px"
+	c.style.background = "url(images/i4.png)"
+	var d = document.createElement('h6')
+	d.innerHTML = "Documentation"
+	d.style.fontSize = "20px"
+	d.style.marginTop = "5px"
+	d.style.marginLeft = "-14px"
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[3].getElementsByTagName('div')[1].appendChild(d)
+
+}
+function goster5(){
+	var a = document.createElement('h1')
+	a.innerHTML = "<b>Quality<b> "
+	a.style.color = "white"
+	a.style.fontSize = "80px"
+	var b = document.createElement('p')
+	b.innerHTML = "We tried to make very high-quality product and so our code is very neat and clean. Whatever anyone could improve and modify the template to your liking."
+	b.style.color = "#999"
+	b.style.fontSize = "26px"
+	b.style.marginTop = "-30px"
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(a)
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(b)
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[4].style.background = "white"
+	var c = document.getElementById('cleanCode').getElementsByClassName('fifthC')[0]
+	c.style.width = "60px"
+	c.style.height = "60px"
+	c.style.marginTop = "50px"
+	c.style.background = "url(images/i5.png)"
+	var d = document.createElement('h6')
+	d.innerHTML = "Quality"
+	d.style.fontSize = "20px"
+	d.style.marginTop = "5px"
+	d.style.marginLeft = "-14px"
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[4].getElementsByTagName('div')[1].appendChild(d)
+
+}
+function goster6(){
+	var a = document.createElement('h1')
+	a.innerHTML = "<b>Support<b> "
+	a.style.color = "white"
+	a.style.fontSize = "80px"
+	var b = document.createElement('p')
+	b.innerHTML = "We tried to make very high-quality product and so our code is very neat and clean. Whatever anyone could improve and modify the template to your liking."
+	b.style.color = "#999"
+	b.style.fontSize = "26px"
+	b.style.marginTop = "-30px"
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(a)
+	document.getElementById('cleanCode').getElementsByClassName('col_12')[0].appendChild(b)
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[5].style.background = "white"
+	var c = document.getElementById('cleanCode').getElementsByClassName('sixthC')[0]
+	c.style.width = "60px"
+	c.style.height = "60px"
+	c.style.marginTop = "50px"
+	c.style.background = "url(images/i6.png)"
+	var d = document.createElement('h6')
+	d.innerHTML = "Support"
+	d.style.fontSize = "20px"
+	d.style.marginTop = "5px"
+	d.style.marginLeft = "-14px"
+	document.getElementById('cleanCode').getElementsByClassName('col_2')[5].getElementsByTagName('div')[1].appendChild(d)
+
+}
+
